f4a2fc04955c8ebcd4f917888383772f142350d7
fetisov <me@efetisov.ru>
2020-11-02 12:48:55 +0300

    update pypi readme

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from distutils.core import setup
-from pathlib import Path
 
+
+from setuptools import setup
 
 exec(open("conveyr/_version.py", encoding="utf-8").read())
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))  
+long_description=None
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()   
 
 setup(
   name = 'conveyr', 
@@ -10,13 +17,15 @@ setup(
   version = __version__,     
   license="MIT -or- Apache License 2.0",
   description="Conveyor pipline handling library for python 3.5 and later",
+  long_description_content_type='text/markdown',
+  long_description=long_description,
   author = 'Evgeniy Fetisov',               
   author_email = 'me@efetisov.ru',  
   url = 'https://github.com/megafetis/mediatr_py',
-  keywords = ['Conveyor', 'Conveyr', 'Mediator','pipline', 'behaviors', 'saga', 'Chain' ], 
+  keywords = ['Conveyor', 'Conveyr','chain handling', 'Mediator','pipline', 'behaviors', 'saga', 'Chain','pattern builder','async conveyor' ], 
   python_requires=">=3.5",
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     "License :: OSI Approved :: MIT License",
